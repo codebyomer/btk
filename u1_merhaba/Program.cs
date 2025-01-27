01f20329311e31012aa428d0d9d2bac5084b1655
codebyomer <omerfarukbalcii35@gmail.com>
2025-01-27 14:11:14 +0300

    asd

---
+++ u1_merhaba/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("BTK AKADEMİ!");
+Console.WriteLine("İzmir 2025");
+Console.WriteLine(2025);
+Console.WriteLine(true);
+Console.WriteLine('A');
---
+++ u2_degiskenler/Program.cs
@@ -0,0 +1,15 @@
+﻿int plaka = 35; //değişkeni tanımlama ve ilk değer verme
+string sehir = "izmir";
+double deger = 125.35;
+
+int a,b=4,c;
+
+a=1;
+c=b;
+
+Console.WriteLine(plaka);
+Console.WriteLine(sehir);
+Console.WriteLine(deger);
+Console.WriteLine(a);
+
+
