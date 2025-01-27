f264b4f51642a575234337c3f2e7c4fa3e6a3f0f
codebyomer <omerfarukbalcii35@gmail.com>
2025-01-27 14:38:28 +0300

    klavyedenokuma

---
+++ u3_klavyedenokuma/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Lütfen adınızı giriniz:");
+string ad = Console.ReadLine();//klavyeden oku
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Merhaba, ");
+Console.WriteLine(ad);
+
+Console.ResetColor();
